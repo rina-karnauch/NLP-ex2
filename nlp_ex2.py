3d7441f6f6b1d144f9fbac8895fd4858016d1118
rinakarnauch <rina.karnauch@mail.huji.ac.il>
2022-11-28 18:42:12 +0200

    cant append item to array

--- nlp_ex2.py
+++ nlp_ex2.py
@@ -2,6 +2,8 @@ import re
 import nltk
 import numpy as np
 from nltk.corpus import brown
+import seaborn as sns
+import matplotlib.pylab as plt
 
 
 def get_dictionaries(train_set, test_set):
@@ -146,11 +148,17 @@ def get_transition(prev, curr, transition_dict):
         return transition_dict[(prev, curr)]
 
 
-def viterbi(sentence, transition_dict, train_tags, train_words, word_tag_counter_dict, tag_counter_dict, delta):
+def get_MLE_tag_index(train_tags_counter_dict, train_tags):
+    max_key = max(train_tags_counter_dict, key=train_tags_counter_dict.get)
+    return np.where(np.array(train_tags) == max_key)[0]
+
+
+def viterbi(sentence, transition_dict, train_tags, words, word_tag_counter_dict, tag_counter_dict, delta):
     m = len(sentence)  # x0,x1,.....,xn,x_(n+1)
     t = len(train_tags)
     pi, bp = np.zeros((m - 1, t)), np.zeros((m - 1, t))
 
+    mle_index = get_MLE_tag_index(tag_counter_dict, train_tags)
     # initial state
     for i_t, tag in enumerate(train_tags):
         pi[0, i_t] = get_transition('START', tag, transition_dict)
@@ -160,16 +168,17 @@ def viterbi(sentence, transition_dict, train_tags, train_words, word_tag_counter
     for w_i, word in enumerate(sentence[1:-1]):
         k = w_i + 1
         for i_u, u_tag in enumerate(train_tags):
-            emission = get_emission(word, u_tag, word_tag_counter_dict, tag_counter_dict, train_words, delta)
-            values = emission * pi[k - 1] * [get_transition(w_tag, u_tag, transition_dict) for w_tag in train_tags]
-            max_index = np.argmax(values)
+            emission = get_emission(word, u_tag, word_tag_counter_dict, tag_counter_dict, words, delta)
+            transition = [get_transition(w_tag, u_tag, transition_dict) for w_tag in train_tags]
+            values = emission * pi[k - 1] * transition
+            max_index = np.argmax(values) if not np.all(values == 0) else mle_index
             pi[k, i_u] = values[max_index]
             bp[k, i_u] = max_index
 
     # get maximal value with STOP transition
     max_tag_index, max_tag_value = -1, -1
     STOP_pi = [pi[-1, i_u] * get_transition(u_tag, 'STOP', transition_dict) for i_u, u_tag in enumerate(train_tags)]
-    curr_max_index = np.argmax(STOP_pi)
+    curr_max_index = np.argmax(STOP_pi) if not np.all(STOP_pi == 0) else mle_index
 
     # trace back
     predicted_tags = np.empty(m - 2, dtype=object)
@@ -285,10 +294,10 @@ def get_pseudo_word(word, count):
             return '__ing__'
     if word[0] == "'" and word[-1] == "'" or word[0] == '"' and word[-1] == '"':
         return '__quote__'
-    return word
+    return '__general__'
 
 
-def solution_e(train_set, test_set):
+def solution_e_1(train_set, test_set):
     train_words_counter_dict, train_tags_counter_dict, \
     train_word_tag_dict, word_counter_dict = get_dictionaries(train_set, test_set)
 
@@ -343,13 +352,105 @@ def solution_e(train_set, test_set):
                     accuracy_unknown += 1
                     accuracy_total += 1
 
-    print("------------ QUESTION E ------------")
-    print("error for known words:", 1 - (accuracy_known / known_count))
-    print("------------------------------------")
-    print("error for unknown words:", 1 - (accuracy_unknown / unknown_count))
+    print("------------ QUESTION E2 ------------")
     print("------------------------------------")
     print("total error:", 1 - (accuracy_total / (known_count + unknown_count)))
     print("------------------------------------")
+
+
+def get_all_tags(train_set, test_set):
+    all_tags = []
+    all_tags=np.array(all_tags)
+    for s in train_set:
+        for w_t in s:
+            np.append(all_tags, [w_t[1]])
+    for s in test_set:
+        for w_t in s:
+            np.append(all_tags, [w_t[1]])
+    return np.unique(all_tags)
+
+
+def get_by_confusion_index(true_tag, predicted_tag, all_tags):
+    i = np.where(all_tags == true_tag)[0]
+    j = np.where(all_tags == predicted_tag)[0]
+    return i, j
+
+
+def solution_e_2(train_set, test_set):
+    train_words_counter_dict, train_tags_counter_dict, \
+    train_word_tag_dict, word_counter_dict = get_dictionaries(train_set, test_set)
+
+    new_train_set, new_test_set = [], []
+    for i_s, s in enumerate(train_set):
+        new_s = []
+        for i_w_t, w_t in enumerate(s):
+            w, t = w_t
+            w_count = train_words_counter_dict.get(w, 0)
+            new_s.append((get_pseudo_word(w, w_count), t))
+        new_train_set.append(new_s)
+    for i_s, s in enumerate(test_set):
+        new_s = []
+        for i_w_t, w_t in enumerate(s):
+            w, t = w_t
+            w_count = train_words_counter_dict.get(w, 0)
+            new_s.append((get_pseudo_word(w, w_count), t))
+        new_test_set.append(new_s)
+
+    test_set, train_set = new_test_set, new_train_set
+    all_tags = get_all_tags(train_set, test_set)
+    c_tags = len(all_tags)
+
+    # upon new words after pseudo conversion, calculate dictionaries
+    train_words_counter_dict, train_tags_counter_dict, \
+    train_word_tag_counter_dict, word_counter_dict = get_dictionaries(train_set, test_set)
+
+    train_tags = list(train_tags_counter_dict.keys())
+    train_words = list(train_words_counter_dict.keys())
+
+    add_padding_to_set(train_set, test_set)
+    # train set and test set were padded with START, STOP
+    test_set_padded, train_set_padded = test_set, train_set
+    transition_dict = get_transition_dict(train_set_padded)
+
+    accuracy_known, known_count = 0, 0
+    accuracy_unknown, unknown_count = 0, 0
+    accuracy_total = 0
+
+    confusion_matrix = np.zeros(shape=(c_tags, c_tags))
+
+    for i, sentence in enumerate(test_set_padded):
+        words = [w_t[0] for w_t in sentence]
+        current_tags = viterbi(words, transition_dict, train_tags, train_words,
+                               train_word_tag_counter_dict, train_tags_counter_dict, delta=1)
+        for w_t_i, w_t in enumerate(sentence[1:-1]):
+            w, t = w_t
+            if w in train_words:
+                known_count += 1
+                if current_tags[w_t_i] == t:
+                    accuracy_known += 1
+                    accuracy_total += 1
+                else:
+                    true_tag, predicted_tag = t, current_tags[w_t_i]
+                    a, b = get_by_confusion_index(true_tag, predicted_tag, np.array(all_tags))
+                    confusion_matrix[a, b] += 1
+            else:
+                unknown_count += 1
+                if current_tags[w_t_i] == t:
+                    accuracy_unknown += 1
+                    accuracy_total += 1
+                else:
+                    true_tag, predicted_tag = t, current_tags[w_t_i]
+                    a, b = get_by_confusion_index(true_tag, predicted_tag, all_tags)
+                    confusion_matrix[a, b] += 1
+
+    print("------------ QUESTION E2 ------------")
+    print("------------------------------------")
+    print("total error:", 1 - (accuracy_total / (known_count + unknown_count)))
+    print("------------------------------------")
+
+    print(confusion_matrix)
+    ax = sns.heatmap(confusion_matrix)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -357,5 +458,5 @@ if __name__ == '__main__':
     # solution_b(train_set, test_set)
     # solution_c(train_set, test_set)
     # solution_d(train_set, test_set)
-    solution_e(train_set, test_set)
-
+    # solution_e_1(train_set, test_set)
+    solution_e_2(train_set, test_set)
